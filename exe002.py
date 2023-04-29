26365fc85e1b34c4d625f4b058122f4aeb11d70c
laisdiasdev <laisgabrielyds@gmail.com>
2023-04-28 22:54:21 -0300

    entrada e saída de dados

---
+++ exe002.py
@@ -0,0 +1,3 @@
+nome = input("Digite seu nome: ")
+idade = int(input("Sua idade: "))
+print("É um prazer te conhecer, {}. {} anos? Bem novo(a)!".format(nome, idade))
---
+++ exe003.py
@@ -0,0 +1,6 @@
+#tipos de dados comuns: int, float, str(caractere), bool
+n1 = int(input("Digite um número: "))
+n2 = int(input("Digite um número: "))
+soma = n1 + n2
+print("A soma de {} + {} é {}".format(n1, n2, soma))
+#revisar a aula 5 ou 6 que fala até quantos caracteres queremos que o usuário digite na input
