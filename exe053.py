21ef94f23e63494f05b8284dbc0509e868786974
laisdiasdev <laisgabrielyds@gmail.com>
2023-05-03 16:19:46 -0300

    estrutura for

--- exe053.py
+++ exe053.py
@@ -1,1 +1,15 @@
 '''Crie um programa que leia uma frase qualquer e diga se ela é um palídromo, disconsiderando os espaços.'''
+frase = input('Digite uma frase qualquer: ').strip().upper()
+palavras = frase.split()
+tudoJunto = ''.join(palavras)
+inverso = ''
+#print('você digitou a frase "{}"'.format(tudoJunto))
+for letra in range(len(tudoJunto) -1, -1, -1): #len(tudoJunto) -1,...) pq precisa ignorar o zero do tamanho
+    inverso += tudoJunto[letra]
+print(tudoJunto, inverso)
+
+if inverso == tudoJunto:
+    print('Temos um palídromo.')
+else:
+    print('Não temos um palídromo.') 
+
